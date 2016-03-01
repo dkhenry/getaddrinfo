b1f295e0edc54d9a70464aa9db1532e7ee1a157e
Koz <dan.kozlowski@gmail.com>
2016-02-29 22:14:37 -0500

    The tests don't SIGSEGV anymore

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@ use std::ptr;
 extern crate libc; 
 use libc::{sockaddr,sockaddr_in,sockaddr_in6,ifaddrs};
 
-#[repr(C)]
+#[repr(C, packed)]
+#[derive(Debug)]
 pub struct in6addrinfo {
     flags: u8,
     prefixlen: u8,
@@ -23,22 +24,25 @@ fn htonl( n: u32 ) -> u32 {
      (((n & 0xFF000000)) >> 24))
 }
 
-unsafe fn ifa_netmask_to_prefix(netmask: libc::sockaddr) -> u8 {
-    match netmask.sa_family as i32 {
+unsafe fn ifa_netmask_to_prefix(netmask: *mut libc::sockaddr) -> u8 {
+    if netmask.is_null() {
+        return 0
+    }
+    match (*netmask).sa_family as i32 {
         libc::AF_INET => {
-            let addr: libc::sockaddr_in = mem::transmute::<libc::sockaddr,libc::sockaddr_in>(netmask);
-            mem::transmute::<libc::in_addr,u32>(addr.sin_addr).count_ones() as u8
+            let addr: *const libc::sockaddr_in = netmask as *const libc::sockaddr_in;
+            mem::transmute::<libc::in_addr,u32>((*addr).sin_addr).count_ones() as u8
         },
         libc::AF_INET6 => {
-            let addr: sockaddr_in6 = *(mem::transmute::<*const libc::sockaddr,*const libc::sockaddr_in6>(&netmask));
-            mem::transmute::<libc::in6_addr,[u8; 16]>(addr.sin6_addr).iter().map(|v| { (v as &u8).count_ones() }).fold(0, |acc, v| { acc + v }) as u8
+            let addr: *const libc::sockaddr_in6 = netmask as *const libc::sockaddr_in6;
+            mem::transmute::<libc::in6_addr,[u8; 16]>((*addr).sin6_addr).iter().map(|v| { (v as &u8).count_ones() }).fold(0, |acc, v| { acc + v }) as u8
         },
         _ => 0 as u8,
     }
 }
 
 unsafe fn sockaddr_to_addr( sock: libc::sockaddr ) -> [u32; 4] {
-    let mut rvalue :[u32; 4] = mem::zeroed();
+    let mut rvalue: [u32; 4] = mem::zeroed();
     match sock.sa_family as i32 {
         libc::AF_INET => {
             let addr: libc::sockaddr_in = mem::transmute::<libc::sockaddr,libc::sockaddr_in>(sock);
@@ -49,7 +53,7 @@ unsafe fn sockaddr_to_addr( sock: libc::sockaddr ) -> [u32; 4] {
         },
         libc::AF_INET6 => {
             let addr: sockaddr_in6 = *(mem::transmute::<*const libc::sockaddr,*const libc::sockaddr_in6>(&sock));
-            ptr::copy(&mem::transmute::<libc::in6_addr,[u32;4]>(addr.sin6_addr),&mut rvalue,4);
+            ptr::copy(&mem::transmute::<libc::in6_addr,[u32;4]>(addr.sin6_addr),&mut rvalue,1);
             rvalue
         },
         _ => rvalue
@@ -59,10 +63,10 @@ unsafe fn sockaddr_to_addr( sock: libc::sockaddr ) -> [u32; 4] {
 unsafe fn ifaddr_to_in6addrinfo( rhs :&libc::ifaddrs ) -> in6addrinfo {
     return in6addrinfo {
         flags: rhs.ifa_flags as u8,
-        prefixlen: ifa_netmask_to_prefix(*rhs.ifa_netmask),
+        prefixlen: ifa_netmask_to_prefix(rhs.ifa_netmask),
         padding: 0u16,
         index: 0u32,
-        addr: sockaddr_to_addr(*rhs.ifa_addr)
+        addr: sockaddr_to_addr(*(rhs.ifa_addr))
     }
 }
 
@@ -70,7 +74,6 @@ unsafe fn ifaddr_to_in6addrinfo( rhs :&libc::ifaddrs ) -> in6addrinfo {
 pub extern "C" fn __check_pf(seen_ipv4: *mut bool, seen_ipv6: *mut bool, in6ai: *mut *mut in6addrinfo, in6ailen: *mut libc::size_t) {
     unsafe { 
         // Call libc::getifaddr
-        
         let mut ifaddrs: *mut libc::ifaddrs = mem::zeroed(); 
 
         let errno = libc::getifaddrs(&mut ifaddrs);
@@ -85,7 +88,7 @@ pub extern "C" fn __check_pf(seen_ipv4: *mut bool, seen_ipv6: *mut bool, in6ai: 
             ifaddrs = (*ifaddrs).ifa_next;                        
         }
         rvalue.push(ifaddr_to_in6addrinfo(&*ifaddrs));
-        
+
         // Format the return value
         *seen_ipv6 = true;
         *seen_ipv4 = true;
@@ -95,7 +98,17 @@ pub extern "C" fn __check_pf(seen_ipv4: *mut bool, seen_ipv6: *mut bool, in6ai: 
     }
 }
 
-
+#[test]
+fn compare_check_pf() {
+    unsafe {
+        let mut ipv4 = false;
+        let mut ipv6 = false;
+        let mut in6ai: *mut in6addrinfo = mem::zeroed();
+        let mut in6ailen = 0;
+        __check_pf(&mut ipv4,&mut ipv6,&mut in6ai, &mut in6ailen);
+        println!("{:?}",in6ai);        
+    }
+}
 
 /// Given a node and a service, which identify as an Internet host and
 /// service, return one or more addrinfo structures each of which
